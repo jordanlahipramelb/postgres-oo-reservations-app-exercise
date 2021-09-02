96777b9b04a4e77332a8f653d7db57148adee993
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2021-09-02 16:58:33 -0700

    initial

---
+++ models/customer.js
@@ -0,0 +1,108 @@
+/** Customer for Lunchly */
+
+const db = require("../db");
+const Reservation = require("./reservation");
+
+/** Customer of the restaurant. */
+
+class Customer {
+  constructor({ id, firstName, lastName, phone, notes }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.notes = notes;
+  }
+
+  /** getting/setting notes
+   *
+   * The set syntax binds an object property to a function to be called when there is an attempt to set that property.
+   */
+  // if someone tries to assign a falsey value to a customer’s notes, the value instead gets assigned to an empty string.
+  set notes(value) {
+    this._notes = value || "";
+  }
+  get notes() {
+    return this._notes;
+  }
+
+  /** get customer's full name
+   *
+   * The get keyword will bind an object property to a function.
+   * When this property is looked up, the getter function is called.
+   * The return value of the getter function then determines which property is returned.
+   * Ex: Used in customer_list.html
+   */
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  /** find all customers. */
+
+  static async all() {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+       FROM customers
+       ORDER BY last_name, first_name`
+    );
+    return results.rows.map((c) => new Customer(c));
+  }
+
+  /** get a customer by ID. */
+
+  static async get(id) {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes 
+        FROM customers WHERE id = $1`,
+      [id]
+    );
+
+    const customer = results.rows[0];
+
+    if (customer === undefined) {
+      const err = new Error(`No such customer: ${id}`);
+      err.status = 404;
+      throw err;
+    }
+
+    return new Customer(customer);
+  }
+
+  /** get all reservations for this customer. */
+
+  async getReservations() {
+    return await Reservation.getReservationsForCustomer(this.id);
+  }
+
+  /** save this customer. */
+
+  async save() {
+    // adds a new customer if they’re new
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO customers (first_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.firstName, this.lastName, this.phone, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      // updates the existing record if there are changes
+      await db.query(
+        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+             WHERE id=$5`,
+        [this.firstName, this.lastName, this.phone, this.notes, this.id]
+      );
+    }
+  }
+}
+
+module.exports = Customer;
